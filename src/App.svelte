88954f41e93e5cc8408a0fe849e55cb6d576a09b
lingeshwaran05 <lingeshkrish08@gmail.com>
2023-02-16 10:55:59 +0530

    adding two numbers completed test 1

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  
+</script>
+
+<main>
+<h1>Adding two numbers</h1>
+<input type="text" name="num1" id="num1" placeholder="Enter first number">
+<p>+</p>
+<input type="text" name="num2" id="num2" placeholder="Enter second number">
+<p>=</p>
+<input type="text" name="result" id="result" placeholder="Result">
+<br>
+<br>
+<button on:click={() => {
+  const num1 = document.getElementById('num1').value;
+  const num2 = document.getElementById('num2').value;
+  const result = document.getElementById('result');
+  result.value = Number(num1) + Number(num2);
+}}>Add</button>
+</main>
+
+<style>
+ 
+
+</style>
